7c907fb84eb0cb75ed21d965392d7c96f3e85784
Yozdel-Alanis <yox009@gmail.com>
2020-06-15 17:05:49 -0500

    Hice scripts para el movimiento del personaje (con un Fixed Update) y del meteorito

---
+++ Assets/Scripts/Meteorite.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteorite : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.down * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+
+    float x;
+    float y;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        //transform.position += new Vector3(x, y, 0f) * speed * Time.deltaTime;
+
+        //rb.velocity = new Vector2(x, y) * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + new Vector3(x, y, 0f) * speed * Time.fixedDeltaTime);
+    }
+}
